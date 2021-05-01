bef6720fab9cdb20a7f4e5c5daac68f0bed43bfa
unknown <shivanandsrinivas1995@gmail.com>
2021-05-01 20:28:01 +0530

    added image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import myImage from './assets/me.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React App</h1>
+        <h1>Hi This is Shivanand !</h1>
+        <div>
+          <a href="https://www.facebook.com/shivanand.satla.71">
+            <img src={myImage} width='100' height='100' className="my-image"/>
+          </a>
+        </div>
       </header>
     </div>
   );
